2601ad6b4f43ca556851838abfe98391ba45e342
ruben medina <ruben@megachopp.com.py>
2020-07-02 07:23:56 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_212220) do
+ActiveRecord::Schema.define(version: 2020_07_02_110426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2020_07_01_212220) do
     t.string "status", default: "no-conciliado"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "bank_id", null: false
-    t.index ["bank_id"], name: "index_transactions_on_bank_id"
+    t.bigint "extracto_id", null: false
+    t.index ["extracto_id"], name: "index_transactions_on_extracto_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,5 +56,5 @@ ActiveRecord::Schema.define(version: 2020_07_01_212220) do
   end
 
   add_foreign_key "extractos", "banks"
-  add_foreign_key "transactions", "banks"
+  add_foreign_key "transactions", "extractos"
 end
